696f2ac2cc285676d374f5bf583cbbf1d065567d
Ida Hallner <bee_ida@hotmail.com>
2019-05-19 16:22:11 +0200

    get the ball to move around

--- Game/script.js
+++ Game/script.js
@@ -40,14 +40,25 @@ function create ()
     // Create paddle, setImmovable to prevent the ball to push the paddle out of the box.
     this.paddle = this.physics.add.image(400, 550, 'assets', 'paddle.png').setImmovable().setCollideWorldBounds(true);
 
+    //Create the ball
+    this.ball = this.physics.add.image(400, 350, 'assets', 'ball.png').setCollideWorldBounds(true);
+
+    //Get the ball to move around
+    this.ball.setBounce(1);
+    this.ball.body.gravity.y = 100;
+    this.ball.body.velocity.set(150);
+
+
+    this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
+    this.physics.add.collider(this.ball, this.paddle, this.hitPaddle, null, this);
+
     //  Event to get the paddle to follow how the mouse move.
     this.input.on('pointermove', function(pointer){
       this.paddle.x = pointer.x;
     }, this);
 
 
-    //Create the ball
-    this.ball = this.physics.add.image(400, 350, 'assets', 'ball.png').setCollideWorldBounds(true);
+
 
 
 }
